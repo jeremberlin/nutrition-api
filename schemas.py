a3fe8be5146cffbde900c915688e189482408aef
jeremberlin <jeremy.stoss.pro@gmail.com>
2025-04-01 11:05:56 +0200

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,1 +1,30 @@
+from pydantic import BaseModel
+from typing import List, Literal
+
+class Recipe(BaseModel):
+    recipe_id: str
+    name: str
+    place: Literal["petit-dejeuner", "entree", "plat"]
+    kcal: float
+    proteines: float
+    lipides: float
+
+class Constraints(BaseModel):
+    kcal: float
+    proteines: float
+    lipides: float
+
+class MealDetail(BaseModel):
+    entree: bool
+    plat: bool
+
+class MealsStructure(BaseModel):
+    petit_dejeuner: int
+    dejeuner: MealDetail
+    diner: MealDetail
+
+class RequestMenu(BaseModel):
+    recipes: List[Recipe]
+    daily_constraints: Constraints
+    meals_structure: MealsStructure
 
